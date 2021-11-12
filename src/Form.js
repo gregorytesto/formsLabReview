8ea3d3f16a15dcc65d57578c6d7e74d3f79ea711
Greg Testo <gregt@pursuit.org>
2021-11-12 11:48:19 -0500

    first commit

--- src/Form.js
+++ src/Form.js
@@ -2,11 +2,73 @@ import React from "react";
 import "./Form.css";
 
 class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: "",
+      operation: ""
+    }
+  }
+
+  handleInputChange=(event)=>{
+    this.setState({
+      input: event.target.value
+    })
+  }
+
+  handleOperationChange=(event)=>{
+    this.setState({
+      operation: event.target.value
+    })
+  }
+
+  calcSum=(arr)=>{
+    let sum = 0;
+    for(let num of arr){
+      sum += num;
+    }
+    return sum;
+  }
+
+  handleSubmit=(event)=>{
+    event.preventDefault();
+    let strArr = this.state.input.split(",");
+    let numArr = strArr.map((str)=>Number(str));
+
+    switch (this.state.operation) {
+      case "sum":
+        let sumResult = this.calcSum(numArr);
+        console.log("Calculate Sum", sumResult);
+        break;
+      case "average":
+        let avgResult = this.calcSum(numArr)/numArr.length;
+        console.log("Calculate Average", avgResult);
+        break;
+      case "mode":
+        console.log("Calculate Mode");
+        break;
+      default:
+        break;
+    }
+    
+  }
+
   render() {
     return (
-      <form>
-        <input id="values" name="values" type="text" />
-        <select id="operation" name="operation">
+      <form onSubmit={this.handleSubmit}>
+        <input 
+          id="values" 
+          name="values" 
+          type="text"
+          value={this.state.input}
+          onInput={this.handleInputChange}
+        />
+        <select 
+          id="operation" 
+          name="operation"
+          value={this.state.operation}
+          onChange={this.handleOperationChange}
+        >
           <option value=""></option>
           <option value="sum">sum</option>
           <option value="average">average</option>
