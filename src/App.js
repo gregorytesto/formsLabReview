d675eac39a45f24c8983041e6e978c0fc25bdc90
Greg Testo <gregt@pursuit.org>
2021-11-12 13:28:47 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import Form from "./Form";
 import "./App.css";
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      displayResult: 0
+    }
+  }
+  handleChangeResult=(result)=>{
+    this.setState({
+      displayResult: result
+    })
+  }
   render() {
     return (
       <main>
         <p>Enter each number in the array, separated by a ','</p>
-        <Form />
+        <Form handleChangeResult={this.handleChangeResult} />
         <section id="result">
-          <p></p>
+          <p>{ this.state.displayResult }</p>
         </section>
       </main>
     );
--- src/Form.js
+++ src/Form.js
@@ -30,27 +30,76 @@ class Form extends React.Component {
     return sum;
   }
 
+  calcMode=(arr)=>{
+    let countObj = {};
+    for(let num of arr){
+      if(countObj[num]){
+        countObj[num] = countObj[num] + 1;
+      } else {
+        countObj[num] = 1;
+      }
+    }
+
+    let keys = Object.keys(countObj);
+    let highestValue=0;
+    let highestKey;
+    for(let key of keys){
+      if(countObj[key] > highestValue){
+        highestValue = countObj[key];
+        highestKey = key;
+      }
+    }
+
+    return highestKey;
+  }
+
+  handleValidation=(numArr)=>{
+    let flag = true;
+    if(this.state.input === ""){
+      flag = false;
+    }
+    for(let num of numArr){
+      if(isNaN(num)){
+        flag=false;
+      }
+    }
+    return flag;
+  }
+
   handleSubmit=(event)=>{
     event.preventDefault();
     let strArr = this.state.input.split(",");
     let numArr = strArr.map((str)=>Number(str));
 
+    let isValid = this.handleValidation(numArr);
+
+    if(this.state.operation === ""){
+      this.props.handleChangeResult("Invalid operation.");
+      return;
+    }
+
+    if(!isValid){
+      this.props.handleChangeResult("Invalid input.");
+      return;
+    }
+
+    let result = 0;
     switch (this.state.operation) {
       case "sum":
-        let sumResult = this.calcSum(numArr);
-        console.log("Calculate Sum", sumResult);
+        result = this.calcSum(numArr);
         break;
       case "average":
-        let avgResult = this.calcSum(numArr)/numArr.length;
-        console.log("Calculate Average", avgResult);
+        result = this.calcSum(numArr)/numArr.length;
         break;
       case "mode":
+        result = this.calcMode(numArr);
         console.log("Calculate Mode");
         break;
       default:
         break;
     }
-    
+
+    this.props.handleChangeResult(result);
   }
 
   render() {
